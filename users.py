4ac904e7cd3970243ee69d66818044a235b913a1
DanaGabitova <58876248+DanaGabitova@users.noreply.github.com>
2022-04-14 17:19:23 +0600

    классы Пользователи и Заявка для бд

---
+++ users.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    application = orm.relation("Applications", back_populates='user')
