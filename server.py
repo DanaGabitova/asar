01af5b41c8423032f53b292c014a309d248ded7a
DanaGabitova <58876248+DanaGabitova@users.noreply.github.com>
2022-04-26 16:33:31 +0300

    перемещение по сайту

--- server.py
+++ server.py
@@ -1,23 +1,38 @@
 from flask import Flask, render_template, redirect
 from loginform import LoginForm
+from registrationform import RegistrationForm
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template('Главная.html')
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('index.html')
-    return render_template('login.html',
-                           form=form)
+        return redirect('/home')
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return redirect('/home')
+    return render_template('signup.html', form=form)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
 
 
 if __name__ == '__main__':
