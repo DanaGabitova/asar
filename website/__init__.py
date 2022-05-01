c5207a7bd2aa291d51f2aa164ed258dd45c68a99
DanaGabitova <58876248+DanaGabitova@users.noreply.github.com>
2022-05-01 07:11:16 +0300

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -30,6 +30,6 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+    if not path.exists(f'website/{os.getenv("DB_NAME")}'):
         db.create_all(app=app)
         print('Created Database!')
