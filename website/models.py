a31b6d56c1c5a3aa04ed1e2fd8fa6def488f4886
TimurKyrbassov <kyrbasovtimur@gmail.com>
2022-05-03 18:39:44 +0600

    Обновил базу данных, добавил в требования к проблеме ее координаты

--- website/models.py
+++ website/models.py
@@ -5,7 +5,8 @@ from sqlalchemy.sql import func
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
+    coordinates = db.Column(db.String(300), unique=True)
+    description = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     status = db.Column(db.String(150))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
