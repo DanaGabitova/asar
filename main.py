0e8aa3aadb8bcaa7b899b4ec37a720bb0c47b536
TimurKyr <95857245+TimurKyr@users.noreply.github.com>
2022-05-07 19:29:55 +0600

    убираю debug..

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ app = create_app()
 if __name__ == '__main__':
     """берем порт из переменной окружения."""
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
